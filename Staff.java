3a57479d0d66958fe7e1e2446c4642ca6b398ab0
eralart <55389823+eralart@users.noreply.github.com>
2022-04-20 21:32:50 +0700

    Mengubah class Employee
    Mengurangi jumlah parameter pada Employee

---
+++ Staff.java
@@ -0,0 +1,12 @@
+package lib;
+
+public class Staff extends TahunGabung {
+	
+	private String employeeId;
+	private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address;
+	
+	
+}
